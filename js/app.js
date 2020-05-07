df114d552f606e89e23d980737b0ada18526f4ed
mark <markpackham1@gmail.com>
2020-05-07 15:30:32 +0100

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript loaded");
+
+  const textInputTitle = document.querySelector("#title");
+  textInputTitle.addEventListener("input", handleInputTitle);
+
+  const textInputAuthor = document.querySelector("#author");
+  textInputAuthor.addEventListener("input", handleInputAuthor);
+
+  const select = document.querySelector("#category");
+  select.addEventListener("change", handleSelect);
+
+  const form = document.querySelector("#new-item-form");
+  form.addEventListener("submit", handleSubmitForm);
+});
+
+const handleInputTitle = function (event) {
+  let title = event.target.value;
+  const resultForm = document.querySelector("#reading-list");
+  // resultForm.textContent = `${title}`;
+};
+
+const handleInputAuthor = function (event) {
+  let author = event.target.value;
+  const resultForm = document.querySelector("#reading-list");
+  // resultForm.textContent = `${author}`;
+};
+
+const handleSelect = function (event) {
+  let selected = event.target.value;
+  const resultForm = document.querySelector("#reading-list");
+  // resultForm.textContent = `${selected}`;
+};
+
+const handleSubmitForm = function (event) {
+  event.preventDefault();
+  const resultForm = document.querySelector("#reading-list");
+  resultForm.textContent = `${this.title.value} ${this.author.value} ${this.selected}`;
+};
